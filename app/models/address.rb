7b66d82b57382d4bd70d974f67c980a70b0ab471
kolyastepanets <kolyastepanets@gmail.com>
2016-02-24 17:09:44 +0200

    edit view settings

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,10 @@ class Address < ActiveRecord::Base
   belongs_to :country
 
   validates :first_name, :last_name, :street, :city, :zip, :phone, :country_id, presence: true
+
+  validates :phone, format: { with: /\A[+-]?\d+?(\.\d+)?\Z/,
+    message: "must have only integers" }
+
   validates_numericality_of :zip
+
 end
