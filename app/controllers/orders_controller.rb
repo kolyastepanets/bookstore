bd00901201c9b0acf0c7de6625d47874c3854a67
kolyastepanets <kolyastepanets@gmail.com>
2016-03-02 19:15:05 +0200

    orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-  load_and_authorize_resource
-
   def index
+    authorize! :index, Order
     @current_order = @order
     @in_processing = current_user.orders.in_processing.order(created_at: :ASC)
     @in_delivery = current_user.orders.in_delivery.order(created_at: :ASC)
@@ -11,7 +10,8 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
+    authorize! :show, Order
+    @old_order = current_user.orders.find(params[:id])
   end
 
 end
